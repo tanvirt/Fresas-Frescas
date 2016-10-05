f87a12fca49a838f6b1ad912c9461e9f5fa22bdd
Tanvir Talukder <tanvirt@ufl.edu>
2016-10-05 11:16:01 -0500

    Created basic login and db connection

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,3 +1,15 @@
 angular.module('main').controller('HomeController', function($scope, $firebaseArray, $state) {
-	$scope.inventory = ['apples', 'oranges', 'bananas'];
+	var ref = firebase.database().ref().child("messages");
+
+	// create a synchronized array
+	$scope.messages = $firebaseArray(ref);
+
+	// add new items to the array
+	// the message is automatically added to our Firebase database!
+	$scope.addMessage = function() {
+		$scope.messages.$add({
+			text: $scope.newMessageText
+		});
+	};
+
 });
