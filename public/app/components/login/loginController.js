ee2b2a3ec311269072033644799b11806201b14b
MARTIN <cm876n@att.com>
2016-10-07 15:06:30 -0500

    Working on login

--- public/app/components/login/loginController.js
+++ public/app/components/login/loginController.js
@@ -2,10 +2,21 @@ angular.module('main').controller('LoginController', function($scope, $firebaseA
 	var auth = $firebaseAuth();
 
 	// login with Facebook
-	auth.$signInWithPopup("google").then(function(firebaseUser) {
-		console.log("Signed in as:", firebaseUser.uid);
-		$state.go('home');
-	}).catch(function(error) {
-		console.log("Authentication failed:", error);
+	// auth.$signInWithPopup("google").then(function(firebaseUser) {
+	// 	console.log("Signed in as:", firebaseUser.uid);
+	// 	$state.go('home');
+	// }).catch(function(error) {
+	// 	console.log("Authentication failed:", error);
+	// });
+
+	$scope.username
+	var email = "bobtony@firebase.com"
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+
+
+		var errorCode = error.code;
+		var errorMessage = error.message;
+
 	});
+	console.log("LOGGGED IN BIIIITCH")
 });
