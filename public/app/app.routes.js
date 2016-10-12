3c12e09900fc51c54f5fe111cb52f5ccdb7921dc
NGO <jn6637@att.com>
2016-10-12 12:53:38 -0500

    404 button routing

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -4,7 +4,7 @@ angular.module('main').config(function($urlRouterProvider, $stateProvider, $loca
 
     $stateProvider
         .state('login', {
-            url: '/login',
+            url: '/',
             templateUrl: 'app/components/login/loginView.html',
             controller: 'LoginController'
         })
--- public/app/components/errorPage/errorController.js
+++ public/app/components/errorPage/errorController.js
@@ -3,4 +3,11 @@ angular.module('main').controller('ErrorController', function($scope, $firebaseA
 	$scope.subheading = "Lost Berries. Please return them to one of these pages.";
 	$scope.headingImage = "../../assets/img/tinted_lost_berries.jpg";
 
+	$scope.goHome = function() {
+		$state.go('home');
+	}
+
+	$scope.goSettings = function() {
+		$state.go('settings');
+	}
 });
