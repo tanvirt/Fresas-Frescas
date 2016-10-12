2cf319f7f48c2c52808a71caceb908db48c7e8a9
NGO <jn6637@att.com>
2016-10-12 10:47:30 -0500

    404 page changes

--- public/app/components/errorPage/errorController.js
+++ public/app/components/errorPage/errorController.js
@@ -1,15 +1,6 @@
 angular.module('main').controller('ErrorController', function($scope, $firebaseArray, $state) {
-	/*var ref = firebase.database().ref().child("messages");
-
-	// create a synchronized array
-	$scope.messages = $firebaseArray(ref);
-
-	// add new items to the array
-	// the message is automatically added to our Firebase database!
-	$scope.addMessage = function() {
-		$scope.messages.$add({
-			text: $scope.newMessageText
-		});
-	};*/
+	$scope.heading = "404";
+	$scope.subheading = "Lost Berries. Please return them to one of these pages.";
+	$scope.headingImage = "../../assets/img/tinted_lost_berries.jpg";
 
 });
