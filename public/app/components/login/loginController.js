793dea36b43a88a91331abf529aa754d6451dd1e
WILDMAN <sw540q@att.com>
2016-10-12 14:48:17 -0500

    added enter login functionality, error for invalid entry, and hidden password

--- public/app/components/login/loginController.js
+++ public/app/components/login/loginController.js
@@ -2,14 +2,52 @@ angular.module('main').controller('LoginController', function($rootScope, $scope
 
 	$scope.authObj = $firebaseAuth();
 
+	$scope.loginError = false;
+
 	$scope.login = function() {
-		$scope.authObj.$signInWithEmailAndPassword($scope.username, $scope.password).then(function(firebaseUser) {
-		  console.log("Signed in as:", firebaseUser.uid);
-		  $rootScope.user = firebaseUser;
-		  console.log($rootScope.user)
-		  $state.go('home');
-		}).catch(function(error) {
-		  console.error("Authentication failed:", error);
-		});
+		try{
+			$scope.authObj.$signInWithEmailAndPassword($scope.username, $scope.password)
+				.then(function(firebaseUser){
+					console.log("Signed in as:", firebaseUser.uid);
+					$rootScope.user = firebaseUser;
+					console.log($rootScope.user)
+					$scope.loginError = false;
+					$state.go('home');
+				}).catch(function(error) {
+			  		$scope.loginError = true;
+			 		console.log("hey");
+			  		console.error("Authentication failed:", error);
+				});
+		}catch(error){
+			$scope.loginError = true;
+			 console.log("hi");
+			  console.error("Authentication failed:", error);
+		}
 	}
+
+
+	loginFailure = function(){
+		$scope.loginError = true;
+		console.log("hey");
+	}
+
+	$scope.getError = function(){
+		return $scope.loginError;
+	}
+
+	document.getElementById("username")
+    .addEventListener("keyup", function(event) {
+    	event.preventDefault();
+    	if (event.keyCode == 13) {
+        	document.getElementById("loginButton").click();
+   		}
+	});
+
+	document.getElementById("password")
+    .addEventListener("keyup", function(event) {
+    	event.preventDefault();
+    	if (event.keyCode == 13) {
+        	document.getElementById("loginButton").click();
+   		}
+	});
 });
