e7ecc41137c8d28c6997d125271aa6a028dbf393
WILDMAN <sw540q@att.com>
2016-10-17 13:27:30 -0500

    added signup form validation

--- public/app/components/signUp/signUpController.js
+++ public/app/components/signUp/signUpController.js
@@ -2,6 +2,18 @@ angular.module('main').controller('SignUpController', function($rootScope, $scop
 
 	$scope.authObj = $firebaseAuth();
 
+	$scope.firstName;
+	$scope.lastName;
+	$scope.username;
+	$scope.password;
+	$scope.confirmPassword;
+
+	$scope.isValid = true;
+
+	$scope.areFieldsValid = function(){
+		return(inputBoxes.firstName.$valid && inputBoxes.lastName.$valid && inputBoxes.username.$valid && inputBoxes.password.$valid);
+	}
+
 	$scope.login = function() {
 		$scope.authObj.$signInWithEmailAndPassword($scope.username, $scope.password).then(function(firebaseUser) {
 		  console.log("Signed in as:", firebaseUser.uid);
@@ -11,5 +23,22 @@ angular.module('main').controller('SignUpController', function($rootScope, $scop
 		}).catch(function(error) {
 		  console.error("Authentication failed:", error);
 		});
+	}
+
+	$scope.signUp = function(){
+		if($scope.password === $scope.confirmPassword && $scope.areFieldsValid()){
+
+			console.log('good');
+
+		$scope.authObj.$createUserWithEmailAndPassword($scope.username, $scope.password) 
+  			.then(function(firebaseUser) { 
+    				console.log("User " + firebaseUser.uid + " created successfully!"); 
+  				}).catch(function(error) { 
+   					 console.error("Error: ", error); 
+ 					});
+  		}
+  		else{
+  			$scope.isValid = false;
+  		}
 	}
 });
