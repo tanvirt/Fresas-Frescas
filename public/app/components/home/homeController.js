a59a99d5bd0d0541e77d6b03e10db370d05a2bc8
Kyle Wahl <kbwahl@neo.tamu.edu>
2016-10-26 15:28:42 -0500

    added recommended projects on home page

--- public/app/components/home/homeController.js
+++ public/app/components/home/homeController.js
@@ -45,6 +45,53 @@ angular.module('main').controller('HomeController', function($rootScope, $scope,
 		views: 234
 	}
 
-	$scope.recommendedProjects = [];
+	$scope.recommendedProjects = [{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	},
+	{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	},
+	{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	},
+	{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	},
+	{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	},
+	{
+		title: "Test Title",
+		description: "I am typing a description here. This is a fantastic project. Good for this person",
+		owners: ["Kyle Wahl", "Tanvir Talukder", "Jason Ngo"],
+		imgSrc: "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+		likes: 77,
+		views: 234
+	}];
 
 });
