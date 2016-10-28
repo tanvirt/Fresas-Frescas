264196bdac30942bb2ff67863b1b6f78a3598896
martinchris6611 <martinchris6611@gmail.com>
2016-10-28 08:48:20 -0500

    Working on project backend

--- public/app/components/projects/createProject/createProjectController.js
+++ public/app/components/projects/createProject/createProjectController.js
@@ -5,14 +5,17 @@ angular.module('main').controller('CreateProjectController', function($rootScope
 	$scope.subheading = "Share your inspiration.";
 	$scope.headingImage = "../../assets/img/ideal_workplace.jpg";
 
+	$scope.authObj = $firebaseAuth();
+	$scope.user = $scope.authObj.$getAuth();
+
 	// Create new project and set defaults
 	$scope.project = {};
-	$scope.project.title = "";
+	$scope.project.title = "Testing Owner";
 	$scope.project.summary = "";
 	$scope.project.details = "";
 	$scope.project.photo = "../../assets/img/modern_workplace.jpg";
-	$scope.project.owners = ['Tanvir Talukder', 'Julia Kieserman'];
-	$scope.project.members = ['Kyle Wahl', 'Christopher Martin', 'Jason Ngo', 'Samuel Wildman'];
+	$scope.project.owners = [];
+	$scope.project.members = [];
 	$scope.project.subscribers = [];
 	$scope.project.tags = ['AngularJS', 'Firebase'];
 	$scope.project.assets = ['ayo', 'random shit', 'dunno how this works'];
@@ -20,10 +23,23 @@ angular.module('main').controller('CreateProjectController', function($rootScope
 	$scope.project.views = 0;
 	$scope.project.creationDate = "";
 
+	//Firebase auth users
+	var userRef = firebase.database().ref().child("users");
+	var userList = $firebaseArray(userRef);
+
+	userList.$loaded().then(function() {
+		angular.forEach(userList, function(aUser) {
+			console.log(aUser);
+			console.log(aUser.$id);
+		})
+	});
+
 	var ref = firebase.database().ref().child("projects");
 
 	$scope.addProjectToDatabase = function() {
 		try {
+			var firebaseUser = $scope.authObj.$getAuth();
+			checkOwners(firebaseUser.uid);
 			ref.child($scope.project.title).set({
 				summary: $scope.project.summary,
 				details: $scope.project.details,
@@ -41,5 +57,41 @@ angular.module('main').controller('CreateProjectController', function($rootScope
 			console.log('Error adding project to DB: ', error);
 		}
 	};
+
+	checkOwners = function(user) {
+		var added = false;
+		angular.forEach($scope.project.owners, function(owner) {
+			if (user == owner) {
+				added = true;
+			}
+		})
+		if (!added) {
+			$scope.project.owners.push(user);
+		}
+	}
+
+	checkMembers = function(user) {
+		var added = false;
+		angular.forEach($scope.project.members, function(member) {
+			if (user == member) {
+				added = true;
+			}
+		})
+		if (!added) {
+			$scope.project.members.push(user);
+		}
+	}
+
+	checkTags = function(newTag) {
+		var added = false;
+		angular.forEach($scope.project.tags, function(tag) {
+			if (newTag == tag) {
+				added = true;
+			}
+		})
+		if (!added) {
+			$scope.project.tags.push(newTag);
+		}
+	}
 
 });
--- public/app/components/projects/myProjects/myProjectsController.js
+++ public/app/components/projects/myProjects/myProjectsController.js
@@ -1,4 +1,4 @@
-angular.module('main').controller('MyProjectsController', function($scope) {
+angular.module('main').controller('MyProjectsController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject) {
 
 	// App header variables
 	$scope.heading = "My Projects";
@@ -9,5 +9,15 @@ angular.module('main').controller('MyProjectsController', function($scope) {
 	$scope.projectsOwning = [];
 	$scope.projectsWorking = [];
 	$scope.projectsFollowing = [];
+
+	var ref = firebase.database().ref().child("projects");
+	var projectList = $firebaseArray(ref);
+
+	projectList.$loaded().then(function() {
+		angular.forEach(projectList, function(project) {
+			console.log(project);
+			console.log(project.views);
+		})
+	});
 
 });
