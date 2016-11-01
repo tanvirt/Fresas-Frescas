ce05495ecedee09d11463c8bf565bd9ba56b62e4
martinchris6611 <martinchris6611@gmail.com>
2016-11-01 16:04:58 -0500

    Beginning search backend

--- public/app/components/search/searchController.js
+++ public/app/components/search/searchController.js
@@ -1,4 +1,4 @@
-angular.module('main').controller('SearchController', function($scope) {
+angular.module('main').controller('SearchController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject) {
 	
 	// App header variables
 	$scope.heading = "Looking For Something?";
@@ -6,5 +6,13 @@ angular.module('main').controller('SearchController', function($scope) {
 	$scope.headingImage = "../../assets/img/world.jpg";
 
 	// Main content starts
+	var searchedString = "Test Project";
+	var ref = firebase.database().ref();
 
+	var projectList = $firebaseArray(ref.child("projects"));
+	var tagList = $firebaseArray(ref.child("tags"));
+	var userList = $firebaseArray(ref.child("users"));
+
+
+	$scope.displayedList = [];
 });
