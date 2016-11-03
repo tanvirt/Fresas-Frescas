c4fa590758c43aaf96ae0552fca07bc0d481a29f
Tanvir Talukder <tanvirt@ufl.edu>
2016-11-03 11:43:15 -0500

    Added cool heading image

--- public/app/components/notifications/notificationsController.js
+++ public/app/components/notifications/notificationsController.js
@@ -1,9 +1,9 @@
 angular.module('main').controller('NotificationsController', function($scope) {
-	
+
 	// App header variables
 	$scope.heading = "View My Notifications";
 	$scope.subheading = "Keep yourself up-to-date.";
-	$scope.headingImage = "../../assets/img/splash.jpg";
+	$scope.headingImage = "../../assets/img/binary.jpg";
 
 	// Main content starts
 
