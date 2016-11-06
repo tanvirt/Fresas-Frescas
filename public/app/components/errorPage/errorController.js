760b97dcd38932a27f85c2bec5859a11e9ee0baa
Kyle Wahl <kbwahl@neo.tamu.edu>
2016-11-06 16:56:49 -0600

    view project button fixes

--- public/app/components/errorPage/errorController.js
+++ public/app/components/errorPage/errorController.js
@@ -1,8 +1,8 @@
 angular.module('main').controller('ErrorController', function($scope, $state) {
 
-	$scope.heading = "404";
-	$scope.subheading = "Lost Berries. Please return them to one of these pages.";
-	$scope.headingImage = "../../assets/img/tinted_lost_berries.jpg";
+	$scope.heading = "Error 404";
+	$scope.subheading = "Error,please return them to one of these pages.";
+	$scope.headingImage = "../../assets/img/404.jpg";
 
 	$scope.goHome = function() {
 		$state.go('home');
